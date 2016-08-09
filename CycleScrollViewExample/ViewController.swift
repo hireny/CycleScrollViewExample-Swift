095a90ca847530d6826d3aeba91da455b562541e
renhai910502500 <renhai910502500@163.com>
2016-08-09 10:18:23 +0800

    Initial Commit

---
+++ CycleScrollViewExample/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CycleScrollViewExample
+//
+//  Created by HX on 16/8/9.
+//  Copyright © 2016年 HX. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
